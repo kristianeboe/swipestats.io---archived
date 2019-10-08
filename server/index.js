5ae8812ec70fca517f9afbaa1346e3064d7f7e43
Kristian Elset Bø <kristian.e.boe@gmail.com>
2019-10-08 18:31:37 +0200

    heroku staging

--- server/index.js
+++ server/index.js
@@ -1,49 +1,46 @@
-const express = require('express')
-const consola = require('consola')
-require('dotenv').config()
+const express = require('express');
+const consola = require('consola');
+require('dotenv').config();
 
-
-
-const { Nuxt, Builder } = require('nuxt')
-const app = express()
+const { Nuxt, Builder } = require('nuxt');
+const app = express();
 
 // Import and Set Nuxt.js options
-const config = require('../nuxt.config.js')
-config.dev = process.env.NODE_ENV !== 'production'
+const config = require('../nuxt.config.js');
+config.dev = process.env.NODE_ENV !== 'production';
 
-app.use(express.json())
+app.use(express.json());
 const apiRoutes = require('./routes/api');
 app.use('/api', apiRoutes);
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('connected to database'))
-
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('connected to database'));
 
 async function start() {
   // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+  const nuxt = new Nuxt(config);
 
-  const { host, port } = nuxt.options.server
+  const { host, port } = nuxt.options.server;
 
   // Build only in dev mode
   if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
+    const builder = new Builder(nuxt);
+    await builder.build();
   } else {
-    await nuxt.ready()
+    await nuxt.ready();
   }
 
   // Give nuxt middleware to express
-  app.use(nuxt.render)
+  app.use(nuxt.render);
 
   // Listen the server
-  app.listen(port, host)
+  app.listen(port, host);
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
-    badge: true
-  })
+    badge: true,
+  });
 }
-start()
+start();
